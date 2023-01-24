3c502c2cb6dd2a8d09643253ef793f931b24e153
Bryant CH <bchihola@gmail.com>
2023-01-25 02:22:59 +0700

    update code

---
+++ src/EmpDetail.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+const EmpDetail = () => {
+    const{empid}=useParams();
+    const[empdata,empdatachange]=useState({})
+
+    useEffect(()=>{
+        fetch("http://localhost:8000/employee/"+empid).then((res)=>{
+            return res.json();
+        }).then((resp)=>{
+            empdatachange(resp);
+        }).catch((err)=>{
+            console.log(err.message)
+        })
+    },[]);
+    return (
+        <div> 
+            <div className="card" style={{"textAlign":"left"}}>
+                <div className="card-title">
+                    <h2>Employee Create</h2>
+                </div>
+                <div className="card-body"></div>
+                { empdata &&
+                    <div>
+                        <h2>The Employee Name Is : {empdata.name} ({empdata.id})</h2>
+                        <h3>Contact Details</h3>
+                        <h5>Email Is : {empdata.email}</h5>
+                        <h5>Phone Is : {empdata.phone}</h5>
+                        <Link className="btn btn-danger" to="/">Back to Listing</Link>
+                    </div>
+                }
+            </div>
+        </div>
+     );
+}
+ 
+export default EmpDetail;
